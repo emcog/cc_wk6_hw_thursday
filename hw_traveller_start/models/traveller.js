ee5137b199ab0550c5377a26f36c68e6cc1c5173
emcog <robin.leonard@gmail.com>
2021-01-08 07:58:04 +0000

    initial enumerators proposed

---
+++ hw_traveller_start/models/traveller.js
@@ -0,0 +1,34 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+// find index
+// ----- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  // ----- find index
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  // filter
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  // filter
+  
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  // sum the contents of an array
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  // possibly slice
+
+};
+
+
+module.exports = Traveller;
